ec483b4a528581e010a6218ff912cb4dc5be81a6
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-02-20 20:36:00 +0530

    Update SideScrolling.cs

--- Super Mario Bros/Assets/Scripts/SideScrolling.cs
+++ Super Mario Bros/Assets/Scripts/SideScrolling.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class SideScrolling : MonoBehaviour
 {
-    
+    private Transform player;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    private void LateUpdate()
+    {
+        Vector3 cameraPosition = transform.position;
+        cameraPosition.x = Mathf.Max(player.position.x, cameraPosition.x);
+        transform.position = cameraPosition;
+
+    }
 }
