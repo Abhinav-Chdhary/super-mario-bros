dfdaaf8cde111aedc8c9deaca4bc8d935a13f440
Abhinav Choudhary <abhinavchdhary@gmail.com>
2023-02-20 20:44:20 +0530

    Update PlayerMovement.cs

--- Super Mario Bros/Assets/Scripts/PlayerMovement.cs
+++ Super Mario Bros/Assets/Scripts/PlayerMovement.cs
@@ -6,10 +6,12 @@ public class PlayerMovement : MonoBehaviour
     private float inputAxis;
     private Vector2 velocity;
     public float moveSpeed = 8.0f;
+    private Camera _camera;
 
     private void Awake()
     {
         _rigidbody= GetComponent<Rigidbody2D>();
+        _camera= Camera.main;
     }
     private void Update()
     {
@@ -24,6 +26,11 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 position = _rigidbody.position;
         position += velocity * Time.fixedDeltaTime;
+
+        //bounds checking
+        Vector2 leftEdge = _camera.WorldToScreenPoint(Vector2.zero);
+        Vector2 rightEdge = _camera.WorldToScreenPoint(new Vector2(Screen.width, Screen.height));
+        position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
 
         _rigidbody.MovePosition(position);
     }
